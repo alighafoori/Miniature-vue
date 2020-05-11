0941c626a3014ac50fa6fba280ea8be7a865c369
ali <ali@DESKTOP-RO0U59K>
2020-05-11 17:22:00 +0430

    Translate all menu

--- src/components/SelectLang/index.jsx
+++ src/components/SelectLang/index.jsx
@@ -4,20 +4,16 @@ import { Icon, Menu, Dropdown } from 'ant-design-vue'
 import { i18nRender } from '@/locales'
 import i18nMixin from '@/store/i18n-mixin'
 
-const locales = ['zh-CN', 'zh-TW', 'en-US', 'pt-BR', 'fa-IR']
+const locales = ['zh-CN', 'en-US', 'fa-IR']
 const languageLabels = {
   'zh-CN': '简体中文',
-  'zh-TW': '繁体中文',
   'en-US': 'English',
-  'pt-BR': 'Português',
   'fa-IR': 'فارسی'
 }
 // eslint-disable-next-line
 const languageIcons = {
   'zh-CN': '🇨🇳',
-  'zh-TW': '🇭🇰',
   'en-US': '🇺🇸',
-  'pt-BR': '🇧🇷',
   'pa-IR': 'ir'
 }
 
