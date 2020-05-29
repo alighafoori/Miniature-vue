e19d3030e94d6aa1ab6b4797f41a0048ea331fd2
ali <ali@DESKTOP-RO0U59K>
2020-05-30 01:49:55 +0430

    translate basic profile view

--- src/views/profile/basic/index.vue
+++ src/views/profile/basic/index.vue
@@ -1,23 +1,23 @@
 <template>
   <page-header-wrapper>
     <a-card :bordered="false">
-      <a-descriptions title="退款申请">
-        <a-descriptions-item label="取货单号">1000000000</a-descriptions-item>
-        <a-descriptions-item label="状态">已取货</a-descriptions-item>
-        <a-descriptions-item label="销售单号">1234123421</a-descriptions-item>
-        <a-descriptions-item label="子订单">3214321432</a-descriptions-item>
+      <a-descriptions :title="$t('basicprofile.refund application')">
+        <a-descriptions-item :label="$t('basicprofile.Pickup number')">1000000000</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.status')">{{ $t('basicprofile.Picked up') }}</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.Sales order number')">1234123421</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.Child order')">3214321432</a-descriptions-item>
       </a-descriptions>
       <a-divider style="margin-bottom: 32px"/>
-      <a-descriptions title="用户信息">
-        <a-descriptions-item label="用户姓名">付小小</a-descriptions-item>
-        <a-descriptions-item label="联系电话">18100000000</a-descriptions-item>
-        <a-descriptions-item label="常用快递">菜鸟仓储</a-descriptions-item>
-        <a-descriptions-item label="取货地址">浙江省杭州市西湖区万塘路18号</a-descriptions-item>
-        <a-descriptions-item label="备注">	无</a-descriptions-item>
+      <a-descriptions :title="$t('basicprofile.User Info')">
+        <a-descriptions-item :label="$t('basicprofile.username')">{{ $t('basicprofile.Fu Xiaoxiao') }}</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.contact number')">18100000000</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.Commonly used express')">{{ $t('basicprofile.Rookie storage') }}</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.Pickup address')">{{ $t('basicprofile.address') }}</a-descriptions-item>
+        <a-descriptions-item :label="$t('basicprofile.Remarks')">	{{ $t('basicprofile.no') }}</a-descriptions-item>
       </a-descriptions>
       <a-divider style="margin-bottom: 32px"/>
 
-      <div class="title">退货商品</div>
+      <div class="title">{{ $t('basicprofile.Returned goods') }}</div>
       <s-table
         style="margin-bottom: 24px"
         row-key="id"
@@ -26,7 +26,7 @@
 
       </s-table>
 
-      <div class="title">退货进度</div>
+      <div class="title">{{ $t('basicprofile.Return progress') }}</div>
       <s-table
         style="margin-bottom: 24px"
         row-key="key"
@@ -46,6 +46,7 @@
 
 <script>
 import { STable } from '@/components'
+import { i18nRender } from '@/locales'
 
 export default {
   components: {
@@ -55,47 +56,47 @@ export default {
     return {
       goodsColumns: [
         {
-          title: '商品编号',
+          title: i18nRender('basicprofile.Product Number'),
           dataIndex: 'id',
           key: 'id'
         },
         {
-          title: '商品名称',
+          title: i18nRender('basicprofile.product name'),
           dataIndex: 'name',
           key: 'name'
         },
         {
-          title: '商品条码',
+          title: i18nRender('basicprofile.barcode'),
           dataIndex: 'barcode',
           key: 'barcode'
         },
         {
-          title: '单价',
+          title: i18nRender('basicprofile.unit price'),
           dataIndex: 'price',
           key: 'price',
           align: 'right'
         },
         {
-          title: '数量（件）',
+          title: i18nRender('basicprofile.Quantity / piece)'),
           dataIndex: 'num',
           key: 'num',
           align: 'right'
         },
         {
-          title: '金额',
+          title: i18nRender('basicprofile.Amount'),
           dataIndex: 'amount',
           key: 'amount',
           align: 'right'
         }
       ],
-      // 加载数据方法 必须为 Promise 对象
+      // The load data method must be a Promise object
       loadGoodsData: () => {
         return new Promise(resolve => {
           resolve({
             data: [
               {
                 id: '1234561',
-                name: '矿泉水 550ml',
+                name: i18nRender('basicprofile.Mineral water 550ml'),
                 barcode: '12421432143214321',
                 price: '2.00',
                 num: '1',
@@ -103,7 +104,7 @@ export default {
               },
               {
                 id: '1234562',
-                name: '凉茶 300ml',
+                name: i18nRender('basicprofile.Herbal tea 300ml'),
                 barcode: '12421432143214322',
                 price: '3.00',
                 num: '2',
@@ -111,7 +112,7 @@ export default {
               },
               {
                 id: '1234563',
-                name: '好吃的薯片',
+                name: i18nRender('basicprofile.Delicious potato chips'),
                 barcode: '12421432143214323',
                 price: '7.00',
                 num: '4',
@@ -119,7 +120,7 @@ export default {
               },
               {
                 id: '1234564',
-                name: '特别好吃的蛋卷',
+                name: i18nRender('basicprofile.Especially delicious egg rolls'),
                 barcode: '12421432143214324',
                 price: '8.50',
                 num: '3',
@@ -138,28 +139,28 @@ export default {
 
       scheduleColumns: [
         {
-          title: '时间',
+          title: i18nRender('basicprofile.time'),
           dataIndex: 'time',
           key: 'time'
         },
         {
-          title: '当前进度',
+          title: i18nRender('basicprofile.Current progress'),
           dataIndex: 'rate',
           key: 'rate'
         },
         {
-          title: '状态',
+          title: i18nRender('basicprofile.status'),
           dataIndex: 'status',
           key: 'status',
           scopedSlots: { customRender: 'status' }
         },
         {
-          title: '操作员ID',
+          title: i18nRender('basicprofile.Operator ID'),
           dataIndex: 'operator',
           key: 'operator'
         },
         {
-          title: '耗时',
+          title: i18nRender('basicprofile.time consuming'),
           dataIndex: 'cost',
           key: 'cost'
         }
@@ -171,41 +172,41 @@ export default {
               {
                 key: '1',
                 time: '2017-10-01 14:10',
-                rate: '联系客户',
+                rate: i18nRender('basicprofile.contact clients'),
                 status: 'processing',
-                operator: '取货员 ID1234',
+                operator: i18nRender('basicprofile.Pickup ID1234'),
                 cost: '5mins'
               },
               {
                 key: '2',
                 time: '2017-10-01 14:05',
-                rate: '取货员出发',
+                rate: i18nRender('basicprofile.Pickup Departure'),
                 status: 'success',
-                operator: '取货员 ID1234',
+                operator: i18nRender('basicprofile.Pickup ID1234'),
                 cost: '1h'
               },
               {
                 key: '3',
                 time: '2017-10-01 13:05',
-                rate: '取货员接单',
+                rate: i18nRender('basicprofile.Pick up the order'),
                 status: 'success',
-                operator: '取货员 ID1234',
+                operator: i18nRender('basicprofile.Pickup ID1234'),
                 cost: '5mins'
               },
               {
                 key: '4',
                 time: '2017-10-01 13:00',
-                rate: '申请审批通过',
+                rate: i18nRender('basicprofile.Application approved'),
                 status: 'success',
-                operator: '系统',
+                operator: i18nRender('basicprofile.system'),
                 cost: '1h'
               },
               {
                 key: '5',
                 time: '2017-10-01 12:00',
-                rate: '发起退货申请',
+                rate: i18nRender('basicprofile.Initiate'),
                 status: 'success',
-                operator: '用户',
+                operator: i18nRender('basicprofile.user'),
                 cost: '5mins'
               }
             ],
@@ -223,9 +224,9 @@ export default {
   filters: {
     statusFilter (status) {
       const statusMap = {
-        'processing': '进行中',
-        'success': '完成',
-        'failed': '失败'
+        'processing': i18nRender('basicprofile.processing'),
+        'success': i18nRender('basicprofile.carry out'),
+        'failed': i18nRender('basicprofile.failure')
       }
       return statusMap[status]
     }
